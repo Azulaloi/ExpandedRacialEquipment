118fed19c418b3ec8fe1707dc2c785fe52e01d11
Azulaloi Champagne <azulaloichampagne@gmail.com>
2020-06-04 17:09:55 -0400

    autopasterizer evolved

--- dev/autopasterize.py
+++ dev/autopasterize.py
@@ -4,25 +4,27 @@ import logging
 import pyperclip
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
+import json
 
 fileDir = ''
 
 def pasterize(filename):
-	f = open(filename, "r")
-	list = [line.rstrip('\n') for line in f]
-	f.close()
-
-	joined = ''.join(list)
-	# joined.replace(" ", "")
-
-	pyperclip.copy(joined)
+	with open(filename, 'r') as data_file:
+		data = json.load(data_file)
+		itemname = data["itemName"]
+		data.pop('itemName', None)
+		du = json.dumps(data)
+	
+	
+	sa = '/spawnitem ' + itemname + ' \'' +  du + ' \' '
+	pyperclip.copy(sa)
 
 class AzHandler(LoggingEventHandler):
 	def __init__(self):
 		super(AzHandler, self).__init__()
 	def on_modified(self, event):
 		if event.is_directory: return
-		logging.info('pasterizing!')
+		logging.info('pasterizing!' + event.src_path)
 		pasterize(event.src_path)
 		
 	def on_created(self, event):
